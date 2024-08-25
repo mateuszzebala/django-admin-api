308aeab752bd73abcffabb128674db9e91a594c9
mateuszzebala <mateusz.zebala.pl@gmail.com>
2024-08-25 11:11:12 +0200

    fix 1.1.1 bug

--- django_admin_page_api/utils.py
+++ django_admin_page_api/utils.py
@@ -3,6 +3,7 @@ from django.http.multipartparser import MultiPartParser
 from django.core.files.uploadhandler import TemporaryFileUploadHandler
 
 def convert_comma_array(comma_array: str) -> list[str]:
+    if not comma_array: return []
     return [element.strip() for element in comma_array.split(',')]
 
 def convert_query_object(query_string: str) -> dict[str, any]:
